0fba08d809e4d5b5cfc2f7b3b65847cd6d90e588
allen <allen@allendeMacBook-Air.local>
2025-12-06 23:43:43 +0800

    make overhead alone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import torch
+from torch.utils.data import DataLoader
+#
 from network import Regression
+
 
 
 
@@ -8,4 +11,8 @@ if __name__ == "__main__":
     #build up model
     model = Regression()
     #set up dataset
-    train_data = nutrition5k()
+    train_dataset = nutrition5k()
+    train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True,num_workers=8, pin_memory=True, drop_last=False)
+    test_dataset = nutrition5k(split='test')
+    test_loader = DataLoader(test_dataset, batch_size=128, shuffle=True,num_workers=8, pin_memory=True, drop_last=False)
+    #
